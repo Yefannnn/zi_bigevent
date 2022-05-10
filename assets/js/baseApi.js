a760a211d2a35491c12be2f8d734c9adc58f5225
Yefan <yefan97114@qq.com>
2022-05-10 19:07:52 +0800

    完成了主页开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,4 +3,24 @@
 $.ajaxPrefilter(function (option) {
     //option是我们配置的ajax
     option.url = 'http://www.liulongbin.top:3007' + option.url
+    //如果发现请求的url地址中有/my/路径，说明这个路径需要提供token这个身份认证信息
+    if (option.url.indexOf('/my/') !== -1) {
+        //确定访问需要权限之后
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //每次请求的时候，都调用complete方法，每次都去验证一下用户的身份是否合法，如果合法就放行，如果不合法就强制清空token值并且跳转到登录页面
+    option.complete =  function(res) {
+        //拿到服务器返回的数据responseJSON ，用responseJSON来判断用户是否强制登录，如果身份验证失败，就强制将页面跳转到登录页面
+        if (res.responseJSON.status === 1 || res.responseJSON.message === '身份认证失败!') {
+            console.log('验证失败了');
+            //一旦请求的时候发现用户身份验证失败，就强制清空token值并且强制跳转
+            localStorage.removeItem('token')
+            location.href = '../../login.html'
+        }
+
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,71 @@
+$(function () {
+
+    getuserinfo()
+
+
+    //点击退出
+    $('#logo_out').on('click', function () {
+        // console.log(1111);
+        layer = layui.layer
+        layer.confirm('确定退出嘛?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //删除本地存储
+            localStorage.removeItem('token')
+            //跳转首页
+            location.href = '../../login.html'
+
+            layer.close(index)
+        });
+    })
+})
+
+
+//请求用户的身份信息
+function getuserinfo() {
+    $.ajax({
+        type: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success(res) {
+            console.log(res);
+            //判断是否请求成功
+            if (res.status !== 0) return layui.layer.msg('登录失败')
+            layui.layer.msg('登录成功')
+            getavator(res)
+        },
+        // complete(res) { 
+        //     //拿到服务器返回的数据responseJSON ，用responseJSON来判断用户是否强制登录，如果身份验证失败，就强制将页面跳转到登录页面
+        //     if (res.responseJSON.status === 1 ||  res.responseJSON.message === '身份认证失败!') {
+        //         console.log('验证失败了');
+        //         //一旦请求的时候发现用户身份验证失败，就强制清空token值并且强制跳转
+        //         localStorage.removeItem('token')
+        //         location.href = '../../login.html'
+        //     }
+
+        // }
+    })
+}
+
+//获取用户头像模块
+function getavator(res) {
+    //展示用户名
+    let username = res.data.username || res.data.nickname
+    $('.welcome').html(`欢迎 ${username}`)
+    //成功登录后，判断用户是否有头像
+    if (res.user_pic == null) {
+        //没有头像
+        let first = username[0].toUpperCase()
+        $('.text-avator').show()
+        $('.avator').hide()
+        $('.text-avator').html(first)
+
+    } else {
+        //有头像
+        $('.avator').prop('src', res.data.user_pic)
+        $('.text-avator').hide()
+    }
+}
